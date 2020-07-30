49f94a6b9d85157614d5e3a17b05b4a2bfd74955
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-30 12:45:13 +0530

    adding the winning and loosing function

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 class GamblerSimulation {
 
@@ -7,9 +8,27 @@ class GamblerSimulation {
     // every bet is of 1$
     final static int bet = 1;
 
+    static int currentBalence = stake;
+
     public static void main(String[] args) {
         System.out.println("stake: " + stake);
         System.out.println("bet: " + bet);
+
+        // calling the function within the if statement
+        if (winOrLoose() == 1) {
+            currentBalence += bet;
+        } else {
+            currentBalence -= bet;
+        }
+        System.out.println("current Balence avliable: " + currentBalence);
+
+    }
+
+    // creating a function to decide the gamble result
+    public static int winOrLoose() {
+        Random r = new Random();
+        int result = r.nextInt(2);
+        return result;
     }
 
 }
